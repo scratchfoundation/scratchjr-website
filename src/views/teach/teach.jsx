f75fc9961b1d7e6f51cd509cee493331f0923ffc
afreel <austin.freel@gmail.com>
2016-04-22 13:57:06 -0400

    required content-nav in learn and teach instead of duplicating code

--- src/views/teach/teach.jsx
+++ src/views/teach/teach.jsx
@@ -4,68 +4,12 @@ var ReactDOM = require('react-dom');
 var NavBar = require('../../components/navbar/navbar.jsx');
 var Footer = require('../../components/footer/footer.jsx');
 
+require('../../../static/js/lib/content-nav.js');
+
 require('./teach.scss');
 
 var Teach = React.createClass({
 	type: 'Teach',
-	componentDidMount: function() {
-
-		// ~~~~ content-nav.js ~~~~~
-
-    $(function() {
-      // add read-more link to related item thumbnail image and title
-      $(".content-section-item-thumbnail-image").wrap(function() {
-        return $(this).parent().siblings().find($(".read-more")).clone().text("");
-      } );
-      $(".content-section-item-title").wrapInner(function() {
-        return $(this).siblings().find($(".read-more")).clone().text("");
-      } );
-
-      // load the correct content based on URL if there is a #hash
-      var hash = location.hash.substr(1);
-      if (hash){
-        if ($('#'+hash+'-section').length) {
-          updateContent(hash+'-section');
-          updateCategory(hash+'-nav');
-        } else {
-          updateContent(hash);
-          var category = $(".read-more#"+hash).closest($(".content-section")).attr('id').replace("section", "nav");
-          updateCategory(category);
-        }
-      }
-    });
-
-    $(document).on('click', '.content-nav-item', function(e) {
-      updateContent($(this).attr('id').replace('nav', 'section'));
-      updateCategory($(this).attr('id'));
-    });
-
-    $(document).on('click', '.read-more', function(e) {
-      updateContent($(this).attr('id')+"-section");
-    });
-
-    function updateContent(sectionId) {
-      var section = $("#"+sectionId);
-      if(section.length)  {
-        $('.content-section').removeClass('content-section-selected');
-        section.addClass('content-section-selected');
-        if($(".content-section-selected.content-subpage").length) {
-          $('html,body').scrollTop($("#content").offset().top);
-        }
-      }
-    }
-
-    function updateCategory(navId) {
-      if($("#"+navId).length)  {
-        $('.content-nav-item').removeClass('content-nav-item-selected');
-        $("#"+navId).addClass('content-nav-item-selected');
-      }
-
-    }
-
-    // ~~~~~~~~~~~~~~~~~~~
-
-	},
 	render: function() {
 		return (
       <div>
