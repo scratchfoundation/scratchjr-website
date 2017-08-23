5383cbeefc4e15ee68b437950bfb0f03d70a9140
chrisgarrity <chrisg@media.mit.edu>
2017-08-23 10:42:33 -0400

    Family Day news feed placeholder
    Will need to create a widget on the ScratchJr twitter account that has the right query.

--- src/views/outreach/news.jsx
+++ src/views/outreach/news.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {Timeline} from 'react-twitter-widgets';
 
 const NewsSection = () => (
     <div
         className="content-section"
         id="news-section"
     >
-        Twitter moment here
+        <div className="content-section-title">
+            ScratchJr Family Days News
+        </div>
+        <div className="content-section-description">
+            See what other people are doing in their Family Days.
+        </div>
+        <div className="fd-news-feed">
+            <Timeline
+                dataSource={{
+                    sourceType: 'widget',
+                    widgetId: '900362519658848256'
+                }}
+                options={{
+                    username: 'Scratch',
+                    width: '600',
+                    height: '800',
+                    chrome: 'noheader nofooter'
+                }}
+            />
+        </div>
     </div>
 );
 export default NewsSection;
