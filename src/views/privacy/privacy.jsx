94dfe2a758fde7616cc9c0c8b2b70b9ca1d4ad1a
Andrew Sliwinski <andrewsliwinski@acm.org>
2018-05-24 08:33:06 -0400

    Update privacy policy

--- src/views/privacy/privacy.jsx
+++ src/views/privacy/privacy.jsx
@@ -11,69 +11,63 @@ const Privacy = () => (
         <div id="content">
             <h1>Privacy Policy</h1>
             <p>
-                We created ScratchJr so that children can create their own
-                interactive stories and games. We understand how important
-                privacy is to our community, especially children and their
-                parents. We wrote this privacy policy to explain what
-                information we collect, how we use it, and what we&apos;re doing
-                to keep it safe. If you have any questions regarding this
-                privacy policy, you can <a href="mailto:info@scratchjr.org">
-                contact us</a>.
+                We understand how important privacy is to our community,
+                especially children and their parents. We wrote this privacy
+                policy to explain what information we collect through the
+                Scratch Jr. mobile application (the &quot;App&quot;), how we use
+                it, and what we&apos;re doing to keep it safe. If you have any
+                questions regarding this privacy policy, you can{' '}
+                <a href="mailto:info@scratchjr.org">contact us</a>.
             </p>
             <h2>What information does ScratchJr collect?</h2>
             <p>
-                We collect some data on where you click and which parts of
-                the app you use. This &apos;click data&apos; (collected through a
-                tool called Google Analytics) helps us figure out ways
-                to improve the app. We also collect information about
-                your geographic location (using IP address and network
-                location) to help us understand where ScratchJr is being used.
+                The Scratch Team is always looking to better understand how
+                Scratch is used around the world. To help support this effort,
+                Scratch only collects anonymous usage information from the
+                ScratchJr app. This information does not include any Personal
+                Information. For purposes of this Privacy Policy, “Personal
+                Information” means any information relating to an identified or
+                identifiable individual.
             </p>
             <p>
-                Although it uses Google Analytics, ScratchJr does not
-                share any information it collects with Google, and Google
-                does not collect any personal identifying information about you.
-            </p>
-            <p>
-                Google Analytics acquires information by installing a &apos;cookie&apos;
-                 on your device. Cookies are small bits of information that
-                 are stored on your device, without any personally
-                 identifiable information.
+                The anonymous usage information we collect includes data about
+                what parts of the app you use and basic information about your
+                network that allows us to roughly estimate what part of the
+                world you are located in. We also collect general information
+                about the device that you are using ScratchJr with, such as
+                make, model, operating system and screen resolution. We do not
+                collect device identifiers, such as advertising IDs, MAC
+                addresses, or IP addresses. We may also ask you what type of
+                location you are using ScratchJr in (e.g. School, Home, etc.).
+                We do not associate any of this information with an identified
+                or identifiable individual.
             </p>
             <h2>How does the ScratchJr Team use the information it collects?</h2>
             <ul>
                 <li>
-                    We may use the information in research studies intended
-                    to improve our understanding of how people learn with
-                    ScratchJr. The results of this research are shared
-                    with educators and researchers through conferences,
-                    journals, and other publications.
+                    We may use anonymous usage information in research studies
+                    intended to improve our understanding of how people learn
+                    with ScratchJr. The results of this research are shared with
+                    educators and researchers through conferences, journals, and
+                    other publications.
                 </li>
                 <li>
                     We analyze the information to understand and improve
-                    ScratchJr, such as determining which sample projects
-                    are most popular, which characters are most used,
-                    and how long users spend in the app.
+                    ScratchJr, such as determining which characters are most
+                    used and how long users spend in the app.
                 </li>
                 <li>
-                    We may disclose some of the information to third-party
-                    service providers that help us perform various support
-                    functions (such as usage analytics). We are satisfied
-                    that these service providers have privacy policies that
-                    restrict them from further disclosing any of your information.
-                </li>
-                <li>
-                    Other than the cases described above, we will never
-                    share personally identifiable information about you
-                    with any other person, company, or organization, except:
+                    We will never share anonymous usage data with any other
+                    person, company, or organization, except:
                     <ul>
                         <li>
-                            As required to comply with our obligations under the law.
+                            As required to comply with our obligations under the
+                            law
                         </li>
                         <li>
                             For technical reasons, if we are required to
-                            transfer the data on our servers to another
-                            location or organization.
+                            transfer the data on our servers to another location
+                            or organization
                         </li>
                     </ul>
                 </li>
