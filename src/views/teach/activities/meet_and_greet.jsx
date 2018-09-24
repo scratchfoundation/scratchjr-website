c85ce51a26331bf60eb9f693abbd7cdfc720da5d
Katya3141 <katya3141@gmail.com>
2018-09-24 09:31:46 -0400

    add print button to activities (#204)
    * add print button to activities

--- src/views/teach/activities/meet_and_greet.jsx
+++ src/views/teach/activities/meet_and_greet.jsx
@@ -3,8 +3,14 @@ import htmlContent from './meet_and_greet.html';
 
 import '../print.css';
 import '../../../components/sectionitem/sectionitem.scss';
+import PrintButton from '../../../components/printbutton/printbutton.jsx';
 
 const MeetAndGreetHtml = () => (
-    <div dangerouslySetInnerHTML={{__html: htmlContent}} /> // eslint-disable-line react/no-danger
+    <div>
+        <div className="printbutton-container">
+            <PrintButton />
+        </div>
+        <div dangerouslySetInnerHTML={{__html: htmlContent}} /> { /* eslint-disable-line react/no-danger */ }
+    </div>
 );
 export default MeetAndGreetHtml;
--- src/views/teach/activities/sunset.jsx
+++ src/views/teach/activities/sunset.jsx
@@ -3,8 +3,14 @@ import htmlContent from './sunset.html';
 
 import '../print.css';
 import '../../../components/sectionitem/sectionitem.scss';
+import PrintButton from '../../../components/printbutton/printbutton.jsx';
 
 const SunsetHtml = () => (
-    <div dangerouslySetInnerHTML={{__html: htmlContent}} /> // eslint-disable-line react/no-danger
+    <div>
+        <div className="printbutton-container">
+            <PrintButton />
+        </div>
+        <div dangerouslySetInnerHTML={{__html: htmlContent}} /> { /* eslint-disable-line react/no-danger */ }
+    </div>
 );
 export default SunsetHtml;
